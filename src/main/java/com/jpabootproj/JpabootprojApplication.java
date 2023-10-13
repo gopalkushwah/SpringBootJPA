0cb2ed9cb6013cd8f54f3a63a65b8878e9264758
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-10-13 22:33:07 +0530

    Saving List of objects

--- src/main/java/com/jpabootproj/JpabootprojApplication.java
+++ src/main/java/com/jpabootproj/JpabootprojApplication.java
@@ -1,5 +1,7 @@
 package com.jpabootproj;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,12 +15,22 @@ public class JpabootprojApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(JpabootprojApplication.class, args);
 		UserRepository userRepository = applicationContext.getBean(UserRepository.class);
-		User user1 = new User();
-		user1.setName("Satyam");
-		user1.setAddress("Bhopal");
-		User save = userRepository.save(user1);
 		
-		System.out.println(save);
+		User user1 = new User();
+		user1.setName("Ram");
+		user1.setAddress("Bhopal");
+		
+		
+		User user2 = new User();
+		user2.setName("Girraj");
+		user2.setAddress("Bhopal");
+		List<User> list = List.of(user1,user2);
+		
+		Iterable<User> saveAll = userRepository.saveAll(list);
+		
+		saveAll.forEach(result -> {
+			System.out.println(result);
+		});
 		
 	}
 
